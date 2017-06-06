e71ed32af967c67eb488383c609113c1348d317c
rvangar <rvangara@walmartlabs.com>
2017-06-06 11:22:59 -0700

    add-stress-test-scripts

---
+++ stress-test/add-commit-platform-design.rb
@@ -0,0 +1,87 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'open-uri'
+require 'fog'
+require 'json'
+require "base64"
+require 'excon'
+require 'net/http'
+
+print "Please enter your API token: "
+@apitoken = gets.chomp
+puts "Welcome #{@apitoken}"
+
+print "Please enter ORG name: "
+@org_name = gets.chomp
+puts "Welcome #{@org_name}"
+
+print "Please enter Platform Text file API token: "
+@add_platform = gets.chomp
+puts "Welcome #{@add_platform}"
+
+print "Please enter Assembly Name: "
+@assembly_name = gets.chomp
+puts "Welcome #{@assembly_name}"
+
+# OneOps endpoints
+@ooenvs="oneops.stg.walmart.com"
+puts "Welcome #{@ooenvs}"
+
+@curl_get_cmd="curl -X GET -k -H 'Content-Type: application/json' -H 'Accept: application/json'"
+@curl_post_cmd="curl -X POST -k -H 'Content-Type: application/json' -H 'Accept: application/json'"
+@curl_put_cmd="curl -X PUT -k -H 'Content-Type: application/json' -H 'Accept: application/json'"
+@curl_delete_cmd="curl -X DELETE -k -H 'Content-Type: application/json' -H 'Accept: application/json'"
+#puts "#{@curl_get_cmd}"
+
+
+def check_for_failure(response, request)
+	if response.nil? || !$?.success?
+		request == "disabling environment" ? (abort "****** failing while #{request} #{response} so aborting ******") : (abort "****** failing while #{request} #{response["errors"]} #{response} so aborting ******")
+	else
+		request == "disabling environment" ? (puts "****** success while #{request} ******") : (puts "****** success while #{request} #{response["errors"]} ******")
+	end
+end
+
+def add_platform_design()
+	response=`#{@curl_post_cmd} -d @#{@add_platform} https://#{@apitoken}\@#{@ooenvs}/#{@org_name}/assemblies/#{@assembly_name}/design/platforms/ 2>&1`
+	if response.class=="Hash"
+		response=JSON.parse(response)
+	end
+	check_for_failure(response, "Add Platfcrm")
+end
+
+add_platform_design
+
+#Extract Design Yaml
+#https://oneops.stg.walmart.com/stgqe/assemblies/usoms/design/extract.yaml
+
+#LOAD design yaml
+#https://oneops.stg.walmart.com/stgqe/assemblies/ravitest403/design/load
+
+#curl -X POST -k -H 'Content-Type: application/json' -H 'Accept: application/json’ -d  {“data”:”/ravi/test.yaml”} https://USERNAME@oneops.stg.walmart.com/stgqe/assemblies/ravitest403/design/load
+#usom://<your-server>/<ORGANIZATION-NAME>/assemblies/<ASSEMBLY-NAME>/design/platforms
+#curl -v -X POST -k -H 'Content-Type: application/json' -H 'Accept: application/json' -d @add-platform.txt https://Nc12qY4UweYBKyesqyRf@oneops.stg.walmart.com/stgqe/assemblies/ravitest403/design/tomcat
+
+
+#working - add platform in design
+#curl -v -X POST -k -H 'Content-Type: application/json' -H 'Accept: application/json' -d @add-platform.txt https://Nc12qY4UweYBKyesqyRf@oneops.stg.walmart.com/stgqe/assemblies/ravitest403/design/platforms/
+#add-platform.txt file
+#{
+#    "cms_dj_ci": {
+#        "comments": "",
+#        "ciName": "tomcatnew",
+#        "ciAttributes": {
+#            "source": "walmartlabs",
+#            "description": "tomcat platform",
+#            "major_version": "1",
+#            "pack": "tomcat",
+#            "version": "1"
+#        }
+#    }
+#}
+
+
+#152446342
+#working COMMIT
+#curl -v -X POST -k -H 'Content-Type: application/json' -H 'Accept: application/json' https://Nc12qY4UweYBKyesqyRf@oneops.stg.walmart.com/stgqe/assemblies/ravitest403/design/releases/152446342/commit/
---
+++ stress-test/list-platforms-design.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'open-uri'
+require 'fog'
+require 'json'
+require "base64"
+require 'excon'
+require 'net/http'
+
+print "Please enter your API token: "
+@apitoken = gets.chomp
+puts "Welcome #{@apitoken}"
+
+print "Please enter ORG name: "
+@org_name = gets.chomp
+puts "Welcome #{@org_name}"
+
+print "Please enter Assembly Name: "
+@assembly_name = gets.chomp
+puts "Welcome #{@assembly_name}"
+
+# OneOps endpoints
+@ooenvs="oneops.stg.walmart.com"
+puts "Welcome #{@ooenvs}"
+
+@curl_get_cmd="curl -s -X GET -k -H 'Content-Type: application/json' -H 'Accept: application/json'"
+@curl_post_cmd="curl -s -X POST -k -H 'Content-Type: application/json' -H 'Accept: application/json'"
+@curl_put_cmd="curl -s -X PUT -k -H 'Content-Type: application/json' -H 'Accept: application/json'"
+@curl_delete_cmd="curl -s -X DELETE -k -H 'Content-Type: application/json' -H 'Accept: application/json'"
+
+def check_for_failure(response, request)
+        if response.nil? || !$?.success?
+                abort "****** failing while #{request} #{response} so aborting ******"
+        else
+                puts "****** success while #{request} ******"
+        end
+end
+
+def get_list_platforms_design()
+        response=JSON.parse(`#{@curl_get_cmd} https://#{@apitoken}\@#{@ooenvs}/#{@org_name}/assemblies/#{@assembly_name}/design/platforms 2>&1`)
+	puts response.class
+	puts response
+	@list_platforms = []
+	response.each do |p|
+	#puts p["ciName"]
+	@list_platforms.push(p["ciName"])
+	end
+	puts @list_platforms
+end
+get_list_platforms_design
+
